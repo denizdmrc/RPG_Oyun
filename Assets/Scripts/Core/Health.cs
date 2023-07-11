16b5715cb6095691a80f4fb1d63ea8ad89f2b4c9
denizdmrc <101872870+denizdmrc@users.noreply.github.com>
2023-07-11 18:41:52 +0300

    Unity ile 3D oyun
    Unity ve C# ile 3D hikayeli oyun

---
+++ Assets/Scripts/Core/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RPG.Core
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] float health = 100f;
+        [SerializeField] UnityEvent onDamage;
+        [SerializeField] UnityEvent onDie;
+
+        bool isDead = false;
+
+        public bool IsDead()
+        {
+            return isDead;
+        }
+        public void TakeDamage(float damage)
+        {
+            health = Mathf.Max(health - damage, 0);
+            onDamage.Invoke();
+            if (health == 0)
+            {
+                onDie.Invoke();
+                Die();
+            }
+        }
+
+        public void Heal(float healToRestore)
+        {
+            health = Mathf.Min(health + healToRestore, 200);
+        }
+
+        private void Die()
+        {
+            if(isDead== true)
+            {
+                return;
+            }
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+    }
+}
+
